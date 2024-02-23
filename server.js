3014d39e47bf7f08e64f9e6445ed4c5a1b13353d
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-23 21:00:27 +0500

    start server

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 import express from "express";
-import http from 'http'
-import socket from "socket.io";
+import http from "http";
+import { Socket } from "socket.io";
 
-const app = express()
-const server = http.createServer(app)
-const io = socket(server)
+const app = express();
+const server = http.createServer(app);
+const io = Socket(server);
 
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+
+    socket.on(
+      "answerCall",
+      (data) => io.to(data.to).emit("callAccepted"),
+      data.signal
+    );
+  });
+});
+
+server.listen(4863, () => console.log("SERVER IS RUUNNIG IN PORT : 4863"));
