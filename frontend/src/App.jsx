588c63c272e7d02eccb2b5b98a3a5e447353e213
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-23 22:16:46 +0500

    imported packages and start app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useRef, useState } from 'react'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import TextField from '@material-ui/core/TextField'
+import AssigmentIcon from '@material-ui/icons/Assessment'
+import PhoneIcon from '@material-ui/icons/Phone'
+import { CopyToClipBoard } from 'react-copy-to-clipboard'
+import Peer from 'simple-peer'
+import io from 'socket.io-client'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const socket = io.connect("http://localhost:4863")
+
+export default function App() {
+
+
+  const [me, setMe] = useState("")
+  const [stream, setStream] = useState()
+  const [receivingCall, setReceivingCall] = useState(false)
+  const [caller, setCaller] = useState("")
+  const [callerSignal, setCallerSignal] = useState()
+  const [callAccepted, setCallAccepted] = useState(false)
+  const [idToCall, setIdToCall] = useState("")
+  const [callEnded, setCallEnded] = useState(false)
+  const [name, setName] = useState("")
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
