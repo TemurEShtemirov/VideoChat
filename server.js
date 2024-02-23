f5ac6193b4971e00cace06e7e08abd28ddf9d628
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-23 21:05:37 +0500

    fixed bug

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import http from "http";
-import { Socket } from "socket.io";
+import { Server } from "socket.io";
+
+
 
 const app = express();
 const server = http.createServer(app);
-const io = Socket(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:9000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
