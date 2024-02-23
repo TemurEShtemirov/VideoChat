1fb89321d421dc1e12cdf9eb1eeaed5fe137baa9
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-23 22:50:23 +0500

    Call Button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import IconButton from '@material-ui/core/IconButton'
 import TextField from '@material-ui/core/TextField'
 import AssigmentIcon from '@material-ui/icons/Assessment'
 import PhoneIcon from '@material-ui/icons/Phone'
-import { CopyToClipBoard } from 'react-copy-to-clipboard'
+import CopyToClipboard from 'react-copy-to-clipboard'
 import Peer from 'simple-peer'
 import io from 'socket.io-client'
 import './App.css'
@@ -49,12 +49,105 @@ export default function App() {
   }, [])
 
 
-const callUser = (id) =>
+  const callUser = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream
+    })
+
+
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name: name
+      })
+
+      peer.on("stream", (stream) => {
+        userVideo.current.srcObject = stream
+      })
+
+      socket.on("callAccepted", (signal) => {
+        setCallAccepted(true)
+        peer.signal(signal)
+      })
+
+
+    })
+
+    connectionRef.current = peer
+
+  }
+
+  const answerCall = () => {
+    setCallAccepted(true)
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream
+    })
+
+
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: caller })
+    })
+
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream
+    })
+
+    peer.signal(callerSignal)
+    connectionRef.current = peer
+
+  }
+
+  const leaveCall = () => {
+    setCallEnded(true)
+    connectionRef.current.destroy()
+  }
 
   return (
     <>
-      <div className="App">
+      <h1 style={{ textAlign: "center", color: "#fff" }}>Zoomler</h1>
+      <div className="container">
+        <div className="video-container">
+          <div className="video">
+            {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+          </div>
+          <div className="video">
+            {callAccepted && !callEnded ?
+              <video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> : null}
+          </div>
+        </div>
+        <div className="myId">
+          <TextField 
+          id="filled-basic"
+          label="Name"
+          variant='filled'
+          value={name}
+          onChange={(e)=> setName(e.target.value)}
+          style={{marginBottom:"20px"}}
+          />
 
+          <CopyToClipboard text={me} style={{marginBottom:"2rem"}} >
+            <Button variant='contained' color='primary' startIcon={<AssigmentIcon fontSize='large'/>}>
+              Copy ID
+            </Button>
+          </CopyToClipboard>
+<TextField 
+id='filled-basic'
+label="ID to Call"
+variant='filled'
+value={idToCall}
+onChange={(e)=> setIdToCall(e.target.value)}
+/>
+
+<div className="call-button">
+  
+</div>
+        </div>
       </div>
     </>
   )
